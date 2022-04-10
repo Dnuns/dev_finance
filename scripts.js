8e17138f2c7a2c6909738a820b1bef1446691df4
David Nunes <51808846+Dnuns@users.noreply.github.com>
2022-04-10 12:28:37 -0100

    add remove transaction

--- scripts.js
+++ scripts.js
@@ -65,12 +65,15 @@ const DOM = {
 
   addTransaction(transaction, index) {
     const tr = document.createElement("tr");
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+    tr.dataset.index = index;
+
+
 
     DOM.transactionsContainer.appendChild(tr);
   },
 
-  innerHTMLTransaction(transaction) {
+  innerHTMLTransaction(transaction, index) {
     const CSSclasses = transaction.amount > 0 ? "income" : "expense";
 
     const amount = Utils.formatCurrency(transaction.amount);
@@ -80,7 +83,7 @@ const DOM = {
         <td class="${CSSclasses}">${amount}</td>
         <td class="date">${transaction.date}</td>
         <td>
-            <img src="./assets/minus.svg" alt="Remover Transação" />
+            <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover Transação" />
         </td>
         `;
 
@@ -182,8 +185,7 @@ const Form = {
 
 const App = {
   init() {
-    Transaction.all.forEach((transaction) => DOM.addTransaction(transaction));
-
+    Transaction.all.forEach(DOM.addTransaction)
     DOM.updateBalance();
   },
   reload() {
